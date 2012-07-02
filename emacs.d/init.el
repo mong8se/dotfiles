3065b08918bccec520d1fff21f939ab3a83096cb
Steven Moazami <smoazami@systemsinmotion.com>
2012-07-02 14:17:15 -0400

    added font in emacs for linux

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -16,8 +16,14 @@
 (setq ido-everywhere t)
 (ido-mode 1)
 
-;; set font
-(set-frame-font "Courier New-20")
+(cond
+  ((eq system-type 'darwin)
+    ;; set font
+    (set-frame-font "Courier New-20"))
+  ((eq system-type 'gnu/linux)
+    ;; set font
+    (set-frame-font "Nimbus Mono L-20"))
+)
 
 ;; Add macports executables to path
 (add-to-list 'exec-path "/opt/local/bin")
