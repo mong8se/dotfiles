ecb02e15d6c41b4deb666ea56a4e284465a22943
Steven Moazami <mong8se@users.noreply.github.com>
2015-01-30 12:31:23 -0500

    refactor zsh plugin to be cleaner

--- zsh/git_named_directory.plugin.zsh
+++ zsh/git_named_directory.plugin.zsh
@@ -1,16 +1,32 @@
+# http://zsh.sourceforge.net/Doc/Release/Expansion.html#Dynamic-named-directories
 zsh_directory_name_functions+=(__git_root_named_directory)
 function __git_root_named_directory ()
 {
-  if [[ $1 = d ]]; then
-    local top_level=$(git -C "$2" rev-parse --show-toplevel 2> /dev/null)
-    if [ $top_level ]; then
-      typeset -ga reply
-      reply=(${top_level##*/} ${#top_level})
-      return
-    else
+  case "$1" in
+    d)
+      # from directory to name
+      local top_level=$(git -C "$2" rev-parse --show-toplevel 2> /dev/null)
+      if [ $top_level ]; then
+        typeset -ga reply
+        reply=(${top_level##*/} ${#top_level})
+        return
+      else
+        return 1
+      fi
+      ;;
+    n)
+      # TODO: not sure if we can go from name to directory
+      # without defining some sort of base path like ~/Projects
       return 1
-    fi
-  else
-    return 1
-  fi
+      ;;
+    c)
+      # TODO: not sure how to do a completion on a name
+      # without defining some sort of base path like ~/Projects
+      return 1
+      ;;
+    *)
+      # here be dragons
+      return 1
+      ;;
+  esac
 }
