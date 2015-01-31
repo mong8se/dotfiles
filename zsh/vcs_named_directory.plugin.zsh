f3a2ac5294b71890481f786360ede83d3e51ecb6
Steven Moazami <mong8se@users.noreply.github.com>
2015-01-30 21:49:46 -0500

    Refactored vcs named dir to slim it down
    Now that it's been changed to be vcs agnostic and rely on the vcs_info
    from zsh, there wouldn't be a way to implement the n and c options of
    the function without having a list of repos which is vcs agnostic.

--- zsh/vcs_named_directory.plugin.zsh
+++ zsh/vcs_named_directory.plugin.zsh
@@ -1,36 +1,19 @@
 # http://zsh.sourceforge.net/Doc/Release/Expansion.html#Dynamic-named-directories
 
-zstyle ':vcs_info:*:__vcs_root:*' formats '%R'
-zstyle ':vcs_info:*:__vcs_root:*' actionformats '%R'
-zsh_directory_name_functions+=(__vcs_root_named_directory)
-
 function __vcs_root_named_directory ()
 {
-  local result=1
-  case "$1" in
-    d)
-      # from directory to name
-      vcs_info __vcs_root
-      if [ $vcs_info_msg_0_ ]; then
-        typeset -ga reply
-        reply=(${vcs_info_msg_0_##*/} ${#vcs_info_msg_0_})
-        result=0
-      fi
-      ;;
-    n)
-      # TODO: not sure if we can go from name to directory
-      # without defining some sort of base path like ~/Projects
-      ;;
-    c)
-      # TODO: not sure how to do a completion on a name
-      # without defining some sort of base path like ~/Projects
-      ;;
-    *)
-      # here be dragons
-      ;;
-  esac
-  if [ $result -eq 0 ]; then
-    vcs_info
+  if [[ $1 = d ]]; then
+    vcs_info __vcs_root
+    if [ $vcs_info_msg_0_ ]; then
+      typeset -ga reply
+      reply=($vcs_info_msg_1_ ${#vcs_info_msg_0_})
+      vcs_info
+      return
+    fi
   fi
-  return $result
+  return 1
 }
+
+zstyle ':vcs_info:*:__vcs_root:*' formats '%R' '%r'
+zstyle ':vcs_info:*:__vcs_root:*' actionformats '%R' '%r'
+zsh_directory_name_functions+=(__vcs_root_named_directory)
