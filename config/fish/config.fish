0b5b7552dcb60971753dccf7083d2af460146576
Steven Moazami <mong8se@users.noreply.github.com>
2015-04-16 11:01:31 -0400

    add fasd to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,11 @@
 #!/bin/env fish
+function -e fish_preexec _run_fasd
+  fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
+end
+
+function z
+  cd (fasd -d -e 'printf %s' "$argv")
+end
 
 function xsource -d "Source list of files if they exist."
   set -l fish_home ~/.config/fish
