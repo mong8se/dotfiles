ad8e1ce121ef90d5f52c535712d40735f69150a9
Steven Moazami <mong8se@users.noreply.github.com>
2015-04-22 09:39:27 -0400

    port more mac-specific functions from zsh to fish

--- config/fish/mac.fish
+++ config/fish/mac.fish
@@ -5,3 +5,17 @@ end
 function make_dark -d "Switch iTerm profile to 'dark'"
   set_iterm_profile 'dark'
 end
+
+function growl -d "Notify"
+  terminal-notifier -message "$argv[1]"
+end
+
+function flush_dns -d "Flush DNS Cache"
+  /usr/bin/dscacheutil -flushcache
+end
+
+function pman -d "Open man page in Preview"
+  man -t $argv | open -f -a /Applications/Preview.app
+end
+
+set -Ux HOMEBREW_GITHUB_API_TOKEN ***REMOVED***
