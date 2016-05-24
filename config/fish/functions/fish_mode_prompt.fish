9514bf24de72008fe8d73a629382b44a5753f4f2
Steven Moazami <mong8se@users.noreply.github.com>
2016-05-24 09:43:19 -0400

    update iterm2 and fish versions

--- config/fish/functions/fish_mode_prompt.fish
+++ config/fish/functions/fish_mode_prompt.fish
@@ -1,20 +1,21 @@
-# The fish_mode_prompt function is prepended to the prompt
 function fish_mode_prompt --description "Displays the current vi mode"
+  # The fish_mode_prompt function is prepended to the prompt
+
   # Do nothing if not in vi mode
-  if set -q __fish_vi_mode
+  if test $__fish_active_key_bindings = "fish_vi_key_bindings"
     switch $fish_bind_mode
       case default
-        set_color --background red white
-        echo ' n '
+        set_color  red
+        echo '║'
       case insert
-        set_color --background green white
-        echo ' i '
+        set_color  green white
+        echo '┃'
       case replace-one
-        set_color --background green white
-        echo ' r '
+        set_color  green
+        echo '╳'
       case visual
-        set_color --background magenta white
-        echo ' v '
+        set_color  magenta
+        echo '┊'
     end
     set_color normal
     # echo -n ' '
