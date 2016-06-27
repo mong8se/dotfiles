8ae2b7940258434dae371773a86912e4ca3df210
Steven Moazami <mong8se@users.noreply.github.com>
2016-06-27 11:15:43 -0400

    circles for fish prompt

--- config/fish/functions/fish_mode_prompt.fish
+++ config/fish/functions/fish_mode_prompt.fish
@@ -1,21 +1,20 @@
 function fish_mode_prompt --description "Displays the current vi mode"
   # The fish_mode_prompt function is prepended to the prompt
-
   # Do nothing if not in vi mode
   if test $__fish_active_key_bindings = "fish_vi_key_bindings"
     switch $fish_bind_mode
       case default
         set_color  red
-        echo '║'
+        echo 'Ⓝ '
       case insert
         set_color  green white
-        echo '┃'
+        echo 'Ⓘ '
       case replace-one
         set_color  green
-        echo '╳'
+        echo 'Ⓡ '
       case visual
         set_color  magenta
-        echo '┊'
+        echo 'Ⓥ '
     end
     set_color normal
     # echo -n ' '
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,25 +1,20 @@
 function fish_prompt --description 'Write out the prompt'
-
   if not set -q __fish_prompt_normal
     set -g __fish_prompt_normal (set_color normal)
   end
-
   switch $USER
-
   case root
     if not set -q __fish_prompt_color
       if set -q fish_color_cwd_root
-        set -g __fish_prompt_color (set_color $fish_color_cwd_root)
+        set -g __fish_prompt_color (set_color $fish_color_cwd_root)'Ⓡ '
       else
-        set -g __fish_prompt_color (set_color $fish_color_cwd)
+        set -g __fish_prompt_color (set_color $fish_color_cwd)'Ⓡ '
       end
     end
-
   case '*'
     if not set -q __fish_prompt_color
-      set -g __fish_prompt_color (set_color $fish_color_history_current)
+      set -g __fish_prompt_color (set_color $fish_color_history_current)'◯ '
     end
   end
-
-  echo -n -s -e "$__fish_prompt_color" '▎' "$__fish_prompt_normal"
+  echo -n -s -e "$__fish_prompt_color" "$__fish_prompt_normal"
 end
