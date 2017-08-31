e6e590759e200397dae1a1d5297ddb2f8ba26d2f
Steven Moazami <mong8se@users.noreply.github.com>
2017-08-31 14:19:47 -0400

    clean up logic for fish prompt
    so we're not doing conditionals every time.
    
    there is an edge case when you have an error right off where the error
    sigil won't have already been set
    
    this is because setting the sigil will change the status code so we
    couldn't detect the error

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,32 +1,43 @@
 function fish_prompt --description 'Write out the prompt'
-  set -l sigil
 
-  if test $status = 0
-    switch $USER
-    case root
+  if test $status -gt 0
+    echo -n -s $__fish_error_sigil
+  else
+    if not set -q __fish_prompt_normal
+      set -g __fish_prompt_normal (set_color normal)
+    end
+
+    if not set -q __fish_root_sigil
       if test $IS_MAC
-        set sigil '🦄 '
-        else
-          set sigil 'u'
-        end
-    case '*'
-      if test $IS_MAC
-        set sigil '🐙 '
+        set -g __fish_root_sigil '🦄 '
       else
-        set sigil 'o'
+        set -g __fish_root_sigil 'u'
       end
     end
-  else
-    if test $IS_MAC
-      set sigil '☠️ '
-    else
-      set sigil 's'
+
+    if not set -q __fish_normal_sigil
+      if test $IS_MAC
+        set -g __fish_normal_sigil '🐙 '
+      else
+        set -g __fish_normal_sigil 'o'
+      end
+    end
+
+    if not set -q __fish_error_sigil
+      if test $IS_MAC
+        set -g __fish_error_sigil '☠️ '
+      else
+        set -g __fish_error_sigil 's'
+      end
+    end
+
+    switch $USER
+    case root
+      echo -n -s $__fish_root_sigil
+    case '*'
+      echo -n -s $__fish_normal_sigil
     end
   end
 
-  if not set -q __fish_prompt_normal
-    set -g __fish_prompt_normal (set_color normal)
-  end
-
-  echo -n -s -e "$sigil " "$__fish_prompt_normal"
+  echo -n -s -e " $__fish_prompt_normal"
 end
