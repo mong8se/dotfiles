22a0bacdeeeb949d6a90eb1fa09853736b10a3f0
Steven Moazami <steven.moazami@dominos.com>
2017-10-13 16:15:25 -0400

    python3 bins added to path

--- config/fish/_07fc97ce7b1e.fish
+++ config/fish/_07fc97ce7b1e.fish
@@ -5,6 +5,6 @@ thefuck --alias | source
 
 set -gx JAVA_HOME (/usr/libexec/java_home -v 1.8)
 set -gx ANDROID_HOME {$HOME}/Library/Android/sdk
-set PATH $PATH {$ANDROID_HOME}/platform-tools {$ANDROID_HOME}/tools
+set PATH $PATH {$ANDROID_HOME}/platform-tools {$ANDROID_HOME}/tools {$HOME}/Library/Python/3.6/bin
 
 set -g fish_user_paths "/usr/local/opt/node@6/bin" $fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
