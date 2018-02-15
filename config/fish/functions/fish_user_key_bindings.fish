83ecd931322b6fe3a2a4ee8f66b96eacc59b9491
Steven Moazami <steven.moazami@dominos.com>
2018-02-15 14:03:29 -0500

    Add bash-like keybindings for !! and !$
    not very fish-like

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,1 +1,28 @@
 fzf_key_bindings
+
+function bind_bang
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+if bind -M insert > /dev/null 2>&1
+  bind -M insert ! bind_bang
+  bind -M insert '$' bind_dollar
+else
+  bind ! bind_bang
+  bind '$' bind_dollar
+end
