c2a8798d4a45eb5763dcb419f9679e469c49b928
Steven Moazami <steven.moazami@dominos.com>
2018-09-18 09:37:10 -0400

    fix zsh config

--- config/zsh/_2e786c488b83.zsh
+++ config/zsh/_2e786c488b83.zsh
@@ -1,4 +1,4 @@
 export ANDROID_HOME=~/Library/Android/sdk
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/tools
 
-source ${HOME}/Projects/olo-vagrant/etc/domprox.sh
+#source ${HOME}/Projects/olo-vagrant/etc/domprox.sh
