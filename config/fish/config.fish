3b9c414c4f822285d4d219457dc4040f035e19fa
Steven Moazami <steven.moazami@dominos.com>
2018-10-10 16:50:56 -0400

    color theme for fzf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -85,4 +85,29 @@ set -x fish_color_autosuggestion brblack --italics
 xsource _(hostname -s | tr -d '\n' | /usr/bin/shasum -p -a 256 | cut -c1-12).fish local.fish
 
 set -x PASSWORD_STORE_DIR ~/Dropbox/.password-store
-set -x FZF_DEFAULT_OPTS '--height 40% --reverse'
+
+# Base16 Gruvbox dark, soft
+# Author: Dawid Kurek (dawikur@gmail.com), morhetz (https://github.com/morhetz/gruvbox)
+set -l color00 '#32302f'
+set -l color01 '#3c3836'
+set -l color02 '#504945'
+set -l color03 '#665c54'
+set -l color04 '#bdae93'
+set -l color05 '#d5c4a1'
+set -l color06 '#ebdbb2'
+set -l color07 '#fbf1c7'
+set -l color08 '#fb4934'
+set -l color09 '#fe8019'
+set -l color0A '#fabd2f'
+set -l color0B '#b8bb26'
+set -l color0C '#8ec07c'
+set -l color0D '#83a598'
+set -l color0E '#d3869b'
+set -l color0F '#d65d0e'
+
+set -x FZF_DEFAULT_OPTS "
+  --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D
+  --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
+  --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D
+  --height 40% --reverse --extended --cycle
+ "
