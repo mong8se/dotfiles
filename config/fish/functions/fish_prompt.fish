cf5000efebacc43274422492712bdf47e831a50a
Steven Moazami <steven.moazami@dominos.com>
2019-08-09 10:13:25 -0400

    refactor code to be easier to read

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,48 +1,55 @@
 function fish_prompt --description 'Write out the prompt'
   if test $status -gt 0
-    if not set -q __fish_prompt_error
-      set -g __fish_prompt_error (set_color normal $fish_color_error)
-    end
-
-    if not set -q __fish_error_sigil
-      if set -q HAS_UTF
-        set -g __fish_error_sigil '☠️'
-      else
-        set -g __fish_error_sigil '*'
-      end
-    end
-
-    printf "%s%s" $__fish_prompt_error $__fish_error_sigil
+    set __prompt_choice "error"
   else
-    if not set -q __fish_prompt_normal
-      set -g __fish_prompt_normal (set_color $fish_color_normal)
-    end
-
-    printf $__fish_prompt_normal
-
-    if not set -q __fish_root_sigil
-      if set -q HAS_UTF
-        set -g __fish_root_sigil '🦄'
-      else
-        set -g __fish_root_sigil '$'
-      end
-    end
-
-    if not set -q __fish_normal_sigil
-      if set -q HAS_UTF
-        set -g __fish_normal_sigil '🐙'
-      else
-        set -g __fish_normal_sigil '|'
-      end
-    end
-
     switch $USER
     case root
-      printf $__fish_root_sigil
+      set __prompt_choice "root"
     case '*'
-      printf $__fish_normal_sigil
+      set __prompt_choice "normal"
     end
   end
 
-  printf "%s "  "$__fish_prompt_normal"
+  if not set -q __fish_prompt_error
+    set -g __fish_prompt_error (set_color normal $fish_color_error)
+  end
+
+  if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color $fish_color_normal)
+  end
+
+  if not set -q __fish_sigil_error
+    if set -q HAS_UTF
+      set -g __fish_sigil_error '☠️'
+    else
+      set -g __fish_sigil_error '*'
+    end
+  end
+
+  if not set -q __fish_sigil_root
+    if set -q HAS_UTF
+      set -g __fish_sigil_root '🦄'
+    else
+      set -g __fish_sigil_root '$'
+    end
+  end
+
+  if not set -q __fish_sigil_normal
+    if set -q HAS_UTF
+      set -g __fish_sigil_normal '🐙'
+    else
+      set -g __fish_sigil_normal '|'
+    end
+  end
+
+  switch $__prompt_choice
+  case "error"
+    printf "%s%s" $__fish_prompt_error $__fish_sigil_error
+  case "root"
+    printf "%s%s" $__fish_prompt_normal $__fish_sigil_root
+  case "*"
+    printf "%s%s" $__fish_prompt_normal $__fish_sigil_normal
+  end
+
+  printf "%s%s" $__fish_prompt_normal " "
 end
