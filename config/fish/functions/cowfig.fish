c1b1e5ca787321e015591a678af7b5a50257fcc8
Steven Moazami <mong8se@users.noreply.github.com>
2019-12-06 13:39:54 -0500

    add function to combine figlet and cowsay and fzf into one command

---
+++ config/fish/functions/cowfig.fish
@@ -0,0 +1,9 @@
+function cowfig -d "Say something through a cow with fzf magic"
+  if status --is-interactive
+    set -l the_text "$argv[1]"
+
+    figlet -f (figlist | rg -v : | sort | fzf +m --prompt="font>" --preview 'figlet -f {} '$the_text) $the_text | cowsay -n -f (cowsay -l | rg -v : | string split ' ' | fzf --prompt="cow>" --preview 'cowsay -f {} '$the_text)
+  else
+    echo "cowfig doing nothing: Non interactive shell"
+  end
+end
