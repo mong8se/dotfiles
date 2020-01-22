37ad28732ffe677722ae29d9d781da7cc8934614
Steven Moazami <mong8se@users.noreply.github.com>
2020-01-22 09:38:32 -0500

    add chrome nacl environment variables

--- config/fish/_74379b6cb323.fish
+++ config/fish/_74379b6cb323.fish
@@ -11,4 +11,5 @@ set -gx TTC_REPOS {$HOME}/Projects
 set -gx TTC_WEATHER "Ann Arbor, MI"
 set -gx TTC_CELSIUS 0
 
-set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" "$HOME/Projects/Chrome/depot_tools"
+set -gx NACL_SDK_ROOT "$HOME/Projects/Chrome/nacl_sdk/pepper_49"
