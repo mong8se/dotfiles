812f81bf69ae7e4150f9f61d2648a53a80dc0988
Steven Moazami <mong8se@users.noreply.github.com>
2020-08-10 10:29:47 -0400

    clean up path and change to node 12

--- config/fish/_3ad2dba9d641.fish
+++ config/fish/_3ad2dba9d641.fish
@@ -5,11 +5,10 @@ thefuck --alias | source
 
 set -gx JAVA_HOME (/usr/libexec/java_home)
 set -gx ANDROID_HOME {$HOME}/Library/Android/sdk
-set PATH $PATH {$ANDROID_HOME}/platform-tools {$ANDROID_HOME}/tools {$HOME}/Library/Python/3.6/bin
 
 set -gx TTC_REPOS {$HOME}/Projects
 set -gx TTC_WEATHER "Ann Arbor, MI"
 set -gx TTC_CELSIUS 0
 
-set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" "$HOME/Projects/Chrome/depot_tools"
+set -g fish_user_paths "/usr/local/opt/node@12/bin" $fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" "$HOME/Projects/Chrome/depot_tools" {$ANDROID_HOME}/platform-tools {$ANDROID_HOME}/tools {$HOME}/Library/Python/3.6/bin
 set -gx NACL_SDK_ROOT "$HOME/Projects/Chrome/nacl_sdk/pepper_49"
