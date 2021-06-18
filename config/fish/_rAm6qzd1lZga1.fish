1a65617d0ccaaafccc90394b5c9f914d2e6bc343
Steven Moazami <mong8se@users.noreply.github.com>
2021-06-18 11:21:33 -0400

    move pumlw to function file

--- config/fish/_rAm6qzd1lZga1.fish
+++ config/fish/_rAm6qzd1lZga1.fish
@@ -13,26 +13,3 @@ set -gx TTC_CELSIUS 0
 set -g fish_user_paths "$HOME/.cargo/bin" "/usr/local/opt/node@12/bin" $fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" "$HOME/Projects/Chrome/depot_tools" {$ANDROID_HOME}/platform-tools {$ANDROID_HOME}/tools {$HOME}/Library/Python/3.6/bin
 set -gx NACL_SDK_ROOT "$HOME/Projects/Chrome/nacl_sdk/pepper_49"
 
-# brew install entr
-function pumlw -d "Watch plantuml and icat output"
-  argparse 't/type=?' -- $argv
-
-  set -l base (string replace .plantuml "" $argv[1])
-  if [ "" = "$_flag_type" ]
-    set _flag_type png
-  end
-
-  set -l icat
-
-  if set -q KITTY_WINDOW_ID
-    set icat "kitty +kitten icat --scale-up"
-  else if set -q ITERM_SESSION_ID
-    set icat imgcat
-    echo $icat
-  else
-    echo Did not detect kitty or iterm2
-    return 1
-  end
-
-  echo $base.plantuml | entr -s "plantuml -t$_flag_type $base.plantuml ; $icat $base.$_flag_type"
-end
---
+++ config/fish/functions/pumlw.fish
@@ -0,0 +1,24 @@
+
+# brew install entr
+function pumlw -d "Watch plantuml and icat output"
+  argparse 't/type=?' -- $argv
+
+  set -l base (string replace .plantuml "" $argv[1])
+  if [ "" = "$_flag_type" ]
+    set _flag_type png
+  end
+
+  set -l icat
+
+  if set -q KITTY_WINDOW_ID
+    set icat "kitty +kitten icat --scale-up"
+  else if set -q ITERM_SESSION_ID
+    set icat imgcat
+    echo $icat
+  else
+    echo Did not detect kitty or iterm2
+    return 1
+  end
+
+  echo $base.plantuml | entr -c -s "plantuml -t$_flag_type $base.plantuml ; $icat $base.$_flag_type"
+end
