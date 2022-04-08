52a607db5f82aab95b18507ae5a7746b42feb4f5
Steven Moazami <mong8se@users.noreply.github.com>
2022-04-08 16:19:10 -0400

    Change Taskfile function
    Changed my mind, instead of always running the
    .dotfiles Taskfile if there is no Taskfile in
    the current directory, only do so if we are in
    the home directory. Otherwise show error

--- config/fish/functions/task.fish
+++ config/fish/functions/task.fish
@@ -1,8 +1,11 @@
 function task -d "Run task from local or home Taskfile"
   if test -f ./Taskfile
-   set -f Taskfile ./Taskfile
+    set -f Taskfile ./Taskfile
+  else if test (pwd) = ~
+    set -f Taskfile ~/.dotfiles/Taskfile
   else
-   set -f Taskfile ~/.dotfiles/Taskfile
+    echo "Taskfile not found."
+    return
   end
 
   bash "$Taskfile" $argv
