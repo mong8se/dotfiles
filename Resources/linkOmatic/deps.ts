cddddf225d8842ea2e382c6a6e937428ab919705
Steven Moazami <mong8se@users.noreply.github.com>
2022-04-26 16:14:42 -0400

    update deno to latest

--- Resources/linkOmatic/deps.ts
+++ Resources/linkOmatic/deps.ts
@@ -5,10 +5,10 @@ export {
   relative,
   resolve,
   isAbsolute
-} from "https://deno.land/std@0.117.0/path/mod.ts";
+} from "https://deno.land/std@0.136.0/path/mod.ts";
 
-export { bold } from "https://deno.land/std@0.113.0/fmt/colors.ts";
+export { bold } from "https://deno.land/std@0.136.0/fmt/colors.ts";
 
-export { sprintf } from "https://deno.land/std@0.113.0/fmt/printf.ts";
+export { sprintf } from "https://deno.land/std@0.136.0/fmt/printf.ts";
 
-export * as log from "https://deno.land/std@0.116.0/log/mod.ts";
+export * as log from "https://deno.land/std@0.136.0/log/mod.ts";
