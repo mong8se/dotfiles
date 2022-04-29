0999a746ca2d8ce966f87426e2bfc04973d90ab7
Steven Moazami <mong8se@users.noreply.github.com>
2022-04-29 12:15:00 -0400

    Add user commmand for split function

--- config/nvim/lua/mong8se.lua
+++ config/nvim/lua/mong8se.lua
@@ -4,61 +4,79 @@ local win = vim.wo
 local mong8se = {}
 
 mong8se.loadRCFiles = function(which)
-  local module = which and function(name)
-    return table.concat({ which, name }, ".")
-  end or function(name) return name end
+    local module = which and
+                       function(name)
+            return table.concat({which, name}, ".")
+        end or function(name) return name end
 
-  for _, name in pairs({'_platform', '_machine', 'local'}) do
-    pcall(require, module(name))
-  end
+    for _, name in pairs({'_platform', '_machine', 'local'}) do
+        pcall(require, module(name))
+    end
 end
 
 -- toggle line numbers
 mong8se.toggleNumberMode = function()
-  if opt.relativenumber then
-    win.number = false
-    win.relativenumber = false
-    win.colorcolumn="0"
-  elseif opt.number then
-    win.relativenumber = true
-    win.colorcolumn="80"
-  else
-    win.number = true
-    win.colorcolumn="80"
-  end
+    if opt.relativenumber then
+        win.number = false
+        win.relativenumber = false
+        win.colorcolumn = "0"
+    elseif opt.number then
+        win.relativenumber = true
+        win.colorcolumn = "80"
+    else
+        win.number = true
+        win.colorcolumn = "80"
+    end
 end
 
 -- scroll bind all windows, works best with vertical splits
 mong8se.toggleScrollBindAllWindows = function()
-  if vim.wo.scrollbind then
-      vim.cmd("windo setlocal noscrollbind")
-  else
-      vim.cmd("windo setlocal scrollbind")
-      vim.cmd("syncbind")
-  end
+    if vim.wo.scrollbind then
+        vim.cmd("windo setlocal noscrollbind")
+    else
+        vim.cmd("windo setlocal scrollbind")
+        vim.cmd("syncbind")
+    end
 end
 
 -- Telescope
 mong8se.activateGitOrFiles = function()
-  local telescope = require("telescope.builtin")
-  if vim.b.git_dir then
-    telescope.git_files()
-  else
-    telescope.find_files()
-  end
+    local telescope = require("telescope.builtin")
+    if vim.b.git_dir then
+        telescope.git_files()
+    else
+        telescope.find_files()
+    end
 end
 
+-- Split either horizontal or vertical, whichever is bigger, arguments
+-- are passed to vim's split command
 mong8se.smartSplit = function(...)
-  local cmd = "split"
+    local cmd = "split"
 
-  if vim.fn.winwidth(0) > vim.fn.winheight(0) * 2 then
-    cmd = "vsplit"
-  end
+    if vim.fn.winwidth(0) > vim.fn.winheight(0) * 2 then cmd = "vsplit" end
 
-  vim.cmd( table.concat({ cmd, ... }, " "))
+    vim.cmd(table.concat({cmd, ...}, " "))
 end
 
--- function! mong8se#ActivateFZF()
+-- Command that uses smart split and by default opens the current directory
+-- instead of default behavior of repeating same buffer
+local function splitCommand(opts)
+    local args = opts.fargs
+
+    if (not args[#args]) or vim.startswith(args[#args], "+") then
+        table.insert(args,
+                     (vim.fn.isdirectory(vim.fn.expand("%")) == 1) and "%" or
+                         "%:h")
+    end
+
+    mong8se.smartSplit(unpack(args))
+end
+
+vim.api.nvim_create_user_command("Split", splitCommand, {nargs = "*"})
+vim.api.nvim_create_user_command("SPlit", splitCommand, {nargs = "*"})
+
+-- function! morng8se#ActivateFZF()
 --   if exists('b:git_dir')
 --     GitFiles
 --   else
