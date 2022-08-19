43090fa241eda2986270a407bbaa09d320c33ad1
Steven Moazami <mong8se@users.noreply.github.com>
2022-08-19 19:02:03 -0400

    change auto rule to match previous commit of changing * to + register

--- home/config/nvim/lua/auto.lua
+++ home/config/nvim/lua/auto.lua
@@ -49,7 +49,7 @@ autocmd("TextYankPost", {
     pattern = "*",
     callback = function()
         if vim.v.event.operator == "y" and vim.v.event.regname == "" then
-            vim.fn.setreg("*", table.concat(vim.v.event.regcontents, "\n"))
+            vim.fn.setreg("+", table.concat(vim.v.event.regcontents, "\n"))
         end
     end,
     group = YankSync
