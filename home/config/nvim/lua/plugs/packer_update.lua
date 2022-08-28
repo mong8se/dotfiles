6fefd2b436c23466a2969c9d6dad963f2421b472
Steven Moazami <mong8se@users.noreply.github.com>
2022-08-28 09:08:05 -0400

    simplify

--- home/config/nvim/lua/plugs/packer_update.lua
+++ home/config/nvim/lua/plugs/packer_update.lua
@@ -1,17 +1,30 @@
 require("plugs.base")
+packer = require('packer')
+
+function wait_for_packer()
+  vim.g._packcomp = 0
+
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'PackerComplete',
+    callback = function() vim.g._packcomp = 1 end
+  })
+
+  vim.wait(5000, function() return vim.g._packcomp == 1 end)
+end
 
 M = {
-    packer_do = function(packer_cmd)
-        packer_cmd = packer_cmd or "PackerSync"
-
-        vim.api.nvim_create_autocmd('User', {
-            pattern = 'PackerComplete',
-            callback = function() vim.g._packcomp = 1 end
-        })
-        vim.g._packcomp = 0
-        vim.cmd(packer_cmd)
-        vim.wait(5000, function() return vim.g._packcomp == 1 end)
-    end
+  install = function()
+    packer.install()
+    wait_for_packer()
+  end,
+  sync = function()
+    packer.sync()
+    wait_for_packer()
+  end,
+  clean = function()
+    packer.clean()
+    wait_for_packer()
+  end
 }
 
 return M
