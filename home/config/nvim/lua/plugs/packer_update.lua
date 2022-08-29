8092e06ff0c636fa80ceaa48759418bd93ec993c
Steven Moazami <mong8se@users.noreply.github.com>
2022-08-28 22:04:52 -0400

    use a closure instead of a global

--- home/config/nvim/lua/plugs/packer_update.lua
+++ home/config/nvim/lua/plugs/packer_update.lua
@@ -2,14 +2,14 @@ require("plugs.base")
 packer = require('packer')
 
 function wait_for_packer()
-  vim.g._packcomp = 0
+  _packcomp = 0
 
   vim.api.nvim_create_autocmd('User', {
     pattern = 'PackerComplete',
-    callback = function() vim.g._packcomp = 1 end
+    callback = function() _packcomp = 1 end
   })
 
-  vim.wait(5000, function() return vim.g._packcomp == 1 end)
+  vim.wait(5000, function() return _packcomp == 1 end)
 end
 
 M = {
