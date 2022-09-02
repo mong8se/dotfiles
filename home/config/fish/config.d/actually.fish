9e7e1a65a9368eee2b5874b8ba8df1e6b33e13b9
Steven Moazami <mong8se@users.noreply.github.com>
2022-09-01 21:12:20 -0400

    add fzf picker to actually.fish

--- home/config/fish/config.d/actually.fish
+++ home/config/fish/config.d/actually.fish
@@ -1,33 +1,49 @@
 function actually -e fish_postexec
   set -f last_status $status
-  if test $last_status -gt 0
-    set -f last_cmd (string split " " $argv[1])
-    if test "cd" = "$last_cmd[1]"
-      # using string replace to do tilde expansion manually, not sure if there is a way
-      # to get it to work automatically when inside a variable from a split string
-      set -f last_dir (path dirname $last_cmd[2] | string replace -r '^~' "$HOME")
-      set -f last_base (path basename $last_cmd[2])
-      set -l list (command find "$last_dir" -maxdepth 1 -mindepth 1 -name "$last_base"'*' | path sort)
+  if test $last_status -eq 0
+    return 0
+  end
 
-      if test "$list" = ""
-        return
-      end
+  set -f last_cmd (string split " " $argv[1])
+  if test "cd" != "$last_cmd[1]"
+    return $last_status
+  end
 
-      set -l i 1
-      set_color blue
-      echo um ... actually, you probably meant:
-      set_color normal
-      for dir in $list
-        echo $i $dir
-        set i (math $i + 1)
-      end
-      set -l answer (read -n 1 -p 'set_color green; echo -n "> " ; set_color normal')
-      if string match -r '^\d+$' "$answer"; and test -n "$list[$answer]" -a -d "$list[$answer]"
-        cd "$list[$answer]"
-      else
-        echo nevermind
-        return 1
-      end
+  # using string replace to do tilde expansion manually, not sure if there is a way
+  # to get it to work automatically when inside a variable from a split string
+  set -f last_dir (path dirname $last_cmd[2] | string replace -r '^~' "$HOME")
+  set -f last_base (path basename $last_cmd[2])
+  set -l list (command find "$last_dir" -maxdepth 1 -mindepth 1 -name "$last_base"'*' | path sort)
+
+  if test -z "$list"
+    return $last_status
+  end
+
+  set_color blue
+  echo um ... actually, you probably meant:
+  set_color normal
+
+  set -f destination
+  if type -q fzf
+    set destination (string join0 $list | fzf --read0 --no-info --no-sort)
+  else
+    set -l i 1
+    for dir in $list
+      echo $i $dir
+      set i (math $i + 1)
     end
+    set -f answer (read -n 1 -p 'set_color green; echo -n "> " ; set_color normal')
+
+    if string match -r '^\d+$' "$answer"
+      and test $answer -gt 0
+      set destination "$list[$answer]"
+    end
+  end
+
+  if test -d "$destination"
+    cd $destination
+  else
+    echo nevermind
+    return $last_status
   end
 end
