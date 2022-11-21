fbd7e3567d1fa919dcbe100951484aeab87570f2
Steven Moazami <mong8se@users.noreply.github.com>
2022-11-21 16:08:55 -0500

    experimental neovim setting

--- home/config/nvim/lua/base.lua
+++ home/config/nvim/lua/base.lua
@@ -21,7 +21,7 @@ settings.wildmode = "longest:full,full"
 settings.wildignore:append("*.o,*.obj,.git,*.rbc,*.class,.svn,vendor/gems/*")
 
 settings.showcmd = true
-settings.cmdheight = 2
+settings.cmdheight = 0
 settings.laststatus = 2
 
 settings.mouse = "nvi"
