1ac2d6a9e24f24ccefd96266a3cfcd925646ef04
Steven Moazami <mong8se@users.noreply.github.com>
2024-07-02 16:03:19 -0400

    update

--- home/config/nvim/lua/bootstrap.lua
+++ home/config/nvim/lua/bootstrap.lua
@@ -1,13 +1,3 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-    lazypath
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- Use lsp for these
 vim.g.polyglot_disabled = {
   "css", "sensible", "go", "html", "javascript", "json", "lua", "python",
@@ -17,11 +7,24 @@ vim.g.polyglot_disabled = {
 -- Golden Ratio
 vim.g.golden_ratio_autocommand = 0
 
+-- Copilot
+vim.g.copilot_no_tab_map = true
+
 local disabled_built_ins = {
   "getscript", "logipat", "2html_plugin", "getscriptPlugin", "matchit", "netrw",
   "netrwPlugin", "rrhelper", "spellfile_plugin", "vimball", "vimballPlugin"
 }
 
 for _, plugin in pairs(disabled_built_ins) do vim.g["loaded_" .. plugin] = 1 end
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugs")
