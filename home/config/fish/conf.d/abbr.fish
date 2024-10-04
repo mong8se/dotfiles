d94cff863fb05f202e1666d33c0d3598fbcbb9dc
Steven Moazami <mong8se@users.noreply.github.com>
2024-10-04 12:02:17 -0400

    move abbrs to a separate file for organization

---
+++ home/config/fish/conf.d/abbr.fish
@@ -0,0 +1,62 @@
+status is-interactive || exit
+
+abbr -a lsg git ls-files
+abbr -a cat cat -v
+abbr -a nrs npm run start
+abbr -a --set-cursor gits git s%
+
+# because half the time I type cd.. instead of cd ..
+function multicd
+  echo cd (string repeat -n (math (string length -- $argv[1]) - 3) ../)
+end
+abbr --add dotdot --regex '^cd\.\.+$' --function multicd
+
+# Imitate Bash !!
+function last_history_item
+  echo $history[1]
+end
+abbr -a !! --position anywhere --function last_history_item
+
+# Imitate Bash !$
+function last_history_arg
+  commandline -f history-token-search-backward
+end
+abbr -a !\$ --position anywhere --function last_history_arg
+
+# Imitate Bash !:1
+function nth_argument
+  string split --fields (math (string sub -s -1 $argv[1]) + 1) " " $history[1]
+end
+abbr --add bangcolon --regex '!:\d' --position anywhere --function nth_argument
+
+# Imitate Bash ^abc^def
+# if ever the abbr regex returns the () matches
+# function replace_last_history; string replace $argv[2] $argv[3] $history[1]; end
+function replace_last_history
+  set -l parts (string split "^" $argv)
+  string replace $parts[2] $parts[3] $history[1]
+end
+abbr --add lastsub --regex '^\^(\w+)\^(\w+)$' --function replace_last_history
+
+# Interactive change
+function change_last_history
+  string replace (string sub -s 2 $argv) % $history[1]
+end
+abbr --add lastchange --regex '^\^(\w+)$' --set-cursor --function change_last_history
+
+# Imitate Zsh =somecommand
+function which_commander
+  string sub -s 2 $argv | xargs command -v
+end
+abbr --add equalcommand --regex "=\w+" --position anywhere --function which_commander
+
+if type -q lsd
+  abbr tree lsd --tree
+  abbr ls lsd
+end
+
+if set -q KITTY_WINDOW_ID
+  alias icat="kitty +kitten icat"
+  alias kg="kitty +kitten hyperlinked_grep"
+  alias ssh="kitty +kitten ssh"
+end
--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -36,57 +36,12 @@ if status --is-interactive
   --height 40% --reverse --extended --cycle
   "
 
-  abbr -a lsg git ls-files
-  abbr -a cat cat -v
-  abbr -a nrs npm run start
-  abbr -a --set-cursor gits git s%
-
-  # because half the time I type cd.. instead of cd ..
-  function multicd
-    echo cd (string repeat -n (math (string length -- $argv[1]) - 3) ../)
-  end
-  abbr --add dotdot --regex '^cd\.\.+$' --function multicd
-
-  # Imitate Bash !!
-  function last_history_item; echo $history[1]; end
-  abbr -a !! --position anywhere --function last_history_item
- 
-  # Imitate Bash !$
-  function last_history_arg; commandline -f history-token-search-backward; end
-  abbr -a !\$ --position anywhere --function last_history_arg
-
-  # Imitate Bash !:1
-  function nth_argument; string split --fields (math (string sub -s -1 $argv[1]) + 1) " " $history[1]; end
-  abbr --add bangcolon --regex '!:\d' --position anywhere --function nth_argument
-
-  # Imitate Bash ^abc^def
-  # if ever the abbr regex returns the () matches
-  # function replace_last_history; string replace $argv[2] $argv[3] $history[1]; end
-  function replace_last_history
-    set -l parts (string split "^" $argv)
-    string replace $parts[2] $parts[3] $history[1]
-  end
-  abbr --add lastsub --regex '^\^(\w+)\^(\w+)$' --function replace_last_history
-
-  # Interactive change
-  function change_last_history
-    string replace (string sub -s 2 $argv) % $history[1]
-  end
-  abbr --add lastchange --regex '^\^(\w+)$' --set-cursor --function change_last_history
-
-  # Imitate Zsh =somecommand
-  function which_commander; string sub -s 2 $argv | xargs command -v;  end
-  abbr --add equalcommand --regex "=\w+" --position anywhere --function which_commander
-
-  if type -q lsd
-    abbr tree lsd --tree
-    abbr ls lsd
-  end
-
-  if set -q KITTY_WINDOW_ID
-    alias icat="kitty +kitten icat"
-    alias kg="kitty +kitten hyperlinked_grep"
-    alias ssh="kitty +kitten ssh"
+  if type -q fd
+    set -x FZF_DEFAULT_COMMAND 'fd --type f --no-ignore'
+    set -x FZF_CTRL_T_COMMAND 'fd --type f . "$dir"'
+  else if type -q rg
+    set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore-vcs --hidden'
+    set -x FZF_CTRL_T_COMMAND 'rg --files . "$dir"'
   end
 
   # Set Base16 Shell Colors
@@ -95,13 +50,6 @@ if status --is-interactive
   set -x fish_color_error brred --italics
   set -x fish_color_autosuggestion brblack --italics
 
-  if type -q fd
-    set -x FZF_DEFAULT_COMMAND 'fd --type f --no-ignore'
-    set -x FZF_CTRL_T_COMMAND 'fd --type f . "$dir"'
-  else if type -q rg
-    set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore-vcs --hidden'
-    set -x FZF_CTRL_T_COMMAND 'rg --files . "$dir"'
-  end
 
   function autoGrowl -d "Auto Growl" -e fish_postexec
     if test $CMD_DURATION -gt 5000
