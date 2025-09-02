c66880b06b6a97a1fce2c48f3e6e983985d377d7
Steven Moazami <mong8se@users.noreply.github.com>
2025-09-02 16:29:25 -0400

    Add more copilot ignored file types

--- home/config/nvim/lua/_fqnIsMx3j1ioi.lua
+++ home/config/nvim/lua/_fqnIsMx3j1ioi.lua
@@ -1,14 +1,1 @@
 vim.g.jsx_ext_required = 0
-
--- copilot
-vim.keymap.set("i", "<right>", 'copilot#Accept("\\<right>")', {
-  expr = true,
-  replace_keycodes = false,
-})
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_filetypes = {
-  ["*"] = true,
-  ["grug-far"] = false,
-  ["grug-far-history"] = false,
-  ["grug-far-help"] = false,
-}
--- home/config/nvim/lua/plugs/_fqnIsMx3j1ioi.lua
+++ home/config/nvim/lua/plugs/_fqnIsMx3j1ioi.lua
@@ -1,7 +1,27 @@
+-- copilot
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_filetypes = {
+  ["*"] = true,
+  ["buffish"] = false,
+  ["grug-far"] = false,
+  ["grug-far-history"] = false,
+  ["grug-far-help"] = false,
+  ["oil"] = false,
+  ["vimwiki"] = false,
+}
+
 return {
   {
     "JamshedVesuna/vim-markdown-preview",
     ft = "markdown",
   },
-  "https://github.com/github/copilot.vim",
+  {
+    "https://github.com/github/copilot.vim",
+    config = function()
+      vim.keymap.set("i", "<right>", 'copilot#Accept("\\<right>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+    end,
+  },
 }
