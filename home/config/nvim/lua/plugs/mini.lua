2403d7d8456df5a9067006d5c6518fb266808720
Steven Moazami <mong8se@users.noreply.github.com>
2025-09-02 14:33:20 -0400

    move github org for mini

--- home/config/nvim/lua/plugs/mini.lua
+++ home/config/nvim/lua/plugs/mini.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "echasnovski/mini.nvim",
+    "nvim-mini/mini.nvim",
     dependencies = "lewis6991/gitsigns.nvim",
     config = function()
       require("mini.bracketed").setup()
