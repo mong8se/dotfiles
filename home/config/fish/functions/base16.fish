c785a166d4d9cfcaf93ff65e4e6121d8cbd34ef1
Steven Moazami <mong8se@users.noreply.github.com>
2025-10-21 10:14:18 -0400

    Cleanup code with switch statement

--- home/config/fish/functions/base16.fish
+++ home/config/fish/functions/base16.fish
@@ -8,15 +8,21 @@ end
 
 complete -c base16 -a "(__base16_schemes)" -d "Color scheme name"
 
+set BASE16_DEFAULT_LIGHT precious-light-warm
+set BASE16_DEFAULT_DARK gruvbox-dark-soft
+
 function base16 -d "Activate base16 terminal color scheme" -a new_theme -a skip_env
   if status --is-interactive
 
-    if test -z "$new_theme"
+    switch "$new_theme"
+    case ''
       set new_theme (__base16_schemes | string split " " | fzf)
-    end
-
-    if test "$new_theme" = random
+    case random
       set new_theme (random choice (__base16_schemes))
+    case light
+      set new_theme $BASE16_DEFAULT_LIGHT
+    case dark
+      set new_theme $BASE16_DEFAULT_DARK
     end
 
     set -l theme_script "$__base16_path/base16-$new_theme.sh"
@@ -25,15 +31,17 @@ function base16 -d "Activate base16 terminal color scheme" -a new_theme -a skip_
       sh $theme_script
       source {$DOTFILES_RESOURCES}/base16-fzf/fish/base16-{$new_theme}.fish
 
+      if type -q vivid
+        set -gx LS_COLORS (vivid generate base16-{$new_theme})
+      end
+
       if test -n "$skip_env"
         set -e BASE16_THEME
       else
         set -xg BASE16_THEME {$new_theme}
       end
 
-      if type -q vivid
-        set -gx LS_COLORS (vivid generate base16-{$new_theme})
-      end
+      return 0
     else
       echo "base16 error: $new_theme not found"
       return 1
@@ -47,10 +55,10 @@ function autoGruv -d "Auto Gruv" -e fish_prompt
   if status --is-interactive && not set -q BASE16_THEME
     if isDarkMode
       set -gx IS_DARK_MODE 1
-      base16 gruvbox-dark-soft false
+      base16 $BASE16_DEFAULT_DARK false
     else
       set -gx IS_DARK_MODE 0
-      base16 equilibrium-light false
+      base16 $BASE16_DEFAULT_LIGHT false
     end
   end
 end
