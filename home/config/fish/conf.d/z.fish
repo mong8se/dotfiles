9d309b0adae4bf4b90e924a8f1fc7b242cd08e75
Steven Moazami <mong8se@users.noreply.github.com>
2025-11-13 11:02:31 -0500

    change from insensitive to smart case for z

--- home/config/fish/conf.d/z.fish
+++ home/config/fish/conf.d/z.fish
@@ -10,7 +10,7 @@ if type -q fre
   end
 
   if not set -q __z_fzf_args
-    set -g __z_fzf_args --nth=-2.. -d / --tiebreak end,length,index --no-sort -i --exact --scheme=path
+    set -g __z_fzf_args --nth=-2.. -d / --tiebreak end,length,index --no-sort --smart-case --exact --scheme=path
   end
 
   function __z_query
