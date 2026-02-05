74a12362cc9b938dd9395c1b30b70efa52a18169
Steven Moazami <mong8se@users.noreply.github.com>
2026-02-05 10:20:26 -0500

    make cusror blink

--- home/config/nvim/lua/base.lua
+++ home/config/nvim/lua/base.lua
@@ -144,3 +144,5 @@ end
 
 settings.grepprg = "rg --vimgrep --no-heading --smart-case"
 settings.grepformat = "%f:%l:%c:%m"
+
+settings.guicursor="n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
