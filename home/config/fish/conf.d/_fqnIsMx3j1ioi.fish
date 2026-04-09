010d2846d9be6fb911288b13300353edc64fa238
Steven Moazami <mong8se@users.noreply.github.com>
2026-04-09 09:35:48 -0400

    default homebrew casks to go into home

--- home/config/fish/conf.d/_fqnIsMx3j1ioi.fish
+++ home/config/fish/conf.d/_fqnIsMx3j1ioi.fish
@@ -5,19 +5,21 @@ if status --is-interactive
   thefuck --alias | source
 end
 
-set -gx JAVA_HOME (/usr/libexec/java_home)
-set -gx ANDROID_HOME {$HOME}/Library/Android/sdk
+set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 
-set -gx TTC_REPOS {$HOME}/Projects
-set -gx TTC_WEATHER "Ann Arbor, MI"
-set -gx TTC_CELSIUS 0
+set -x JAVA_HOME (/usr/libexec/java_home)
+set -x ANDROID_HOME {$HOME}/Library/Android/sdk
+
+set -x TTC_REPOS {$HOME}/Projects
+set -x TTC_WEATHER "Ann Arbor, MI"
+set -x TTC_CELSIUS 0
 
 fish_add_path --append "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" "$HOME/Projects/Chrome/depot_tools" {$ANDROID_HOME}/platform-tools {$ANDROID_HOME}/tools {$HOME}/Library/Python/3.6/bin
 
-set -gx NACL_SDK_ROOT "$HOME/Projects/Chrome/nacl_sdk/pepper_49"
+set -x NACL_SDK_ROOT "$HOME/Projects/Chrome/nacl_sdk/pepper_49"
 
 set -a Z_FALLBACKS ~/Projects/unvagrant/workspace
 
-set -gx NODE_EXTRA_CA_CERTS ~/Projects/unvagrant/certs/dpzca.pem
+set -x NODE_EXTRA_CA_CERTS ~/Projects/unvagrant/certs/dpzca.pem
 
 launchctl setenv NODE_EXTRA_CA_CERTS $NODE_EXTRA_CA_CERTS
