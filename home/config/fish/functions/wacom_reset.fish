f1db3bb9bbf3139bc03a55135b57a3e10d8e46d9
Steven Moazami <mong8se@users.noreply.github.com>
2026-06-02 09:14:11 -0400

    add error handling to wacom_reset

--- home/config/fish/functions/wacom_reset.fish
+++ home/config/fish/functions/wacom_reset.fish
@@ -3,6 +3,5 @@
 function wacom_reset -d "Restart Wacom Driver"
     # killall WacomTabletDriver
     echo -n "Restarting Wacom Driver ..."
-    "/Applications/Wacom Tablet.localized/Wacom Tablet Utility.app/Contents/MacOS/Wacom Tablet Utility" --restart
-    echo "... Done."
+    "/Applications/Wacom Tablet.localized/Wacom Tablet Utility.app/Contents/MacOS/Wacom Tablet Utility" --restart; and echo "... Done."; or echo "... Failed."
 end
