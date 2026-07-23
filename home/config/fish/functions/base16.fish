5299be18f3ce66fbd2e9c5f5841f54f9b990d5d6
Steven Moazami <mong8se@users.noreply.github.com>
2026-07-23 16:16:30 -0400

    change default light scheme

--- home/config/fish/functions/base16.fish
+++ home/config/fish/functions/base16.fish
@@ -3,7 +3,7 @@ if not set -q __base16_path
 end
 
 if not set -q __base16_default_light
-  set -g __base16_default_light selenized-light
+  set -g __base16_default_light penumbra-light-contrast-plus-plus
 end
 
 if not set -q __base16_default_dark
