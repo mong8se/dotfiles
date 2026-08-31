f167d9afdd04d8c87f04c67a997315975832f315
Steven Moazami <mong8se@users.noreply.github.com>
2026-08-31 14:04:52 -0400

    fix shebang

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -1,9 +1,9 @@
+#!/usr/bin/env fish
+
 # The following lines were added by Docker Desktop to add commands to your PATH.
 # export PATH="$PATH:/Users/moazams/.docker/bin"
 fish_add_path --append ~/.docker/bin
 # End of Docker Desktop section.
-
-#!/usr/bin/env fish
 
 if not set -q hostname
   set -g hostname (hostname -s)
